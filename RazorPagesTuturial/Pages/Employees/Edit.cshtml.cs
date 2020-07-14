8918e33cb29b0cbe53ddc1cb1f34a53acac0caf0
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-07-14 11:11:51 -0700

    Latest code

---
+++ RazorPagesTuturial/Pages/Employees/Edit.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Services;
+using RazorPagesTuturialModels;
+
+namespace RazorPagesTuturial.Pages.Employees
+{
+    public class EditModel : PageModel
+    {
+        private readonly IEmployeeRepository employeeRepository;
+        public EditModel(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        public Employee Employee { get; set; }
+
+        public void OnGet(int id)
+        {
+            employeeRepository.GetAllEmployees();
+        }
+    }
+}
